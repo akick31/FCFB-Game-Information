df8e05ceba87327a5ed06a0188c542049b4f798a
Andrew Kicklighter <apkicklighter@gmail.com>
2023-06-04 03:03:32 -0500

    Add logging for game play updater

--- fcfb/games/manage_plays.py
+++ fcfb/games/manage_plays.py
@@ -23,7 +23,12 @@ async def add_ongoing_game_plays(r, config_data, logger):
     games_in_table = await get_all_rows_in_table(config_data, "ongoing_games", logger)
     if games_in_table and games_in_table is not None:
         for game in games_in_table:
+            game_id = game[0]
+            winning_team = game[1]
+            losing_team = game[2]
             subdivision = game[27]
             if game is not None:
                 await add_game_plays(r, config_data, season, subdivision, game, "plays", logger)
-
+                logger.info("Updated " + subdivision + " game " + game_id +
+                            " in the table between " + winning_team + " and "
+                            + losing_team)
